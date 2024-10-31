04eae3ac14a887841434abf3c521dcbdc5921847
ZhiHuangLau <lauzhihuang@gmail.com>
2024-10-31 17:31:55 +0800

    Adds the human response to the script
    This commit adds the prompt for the player to make their choice.
    The code will only accept the correct response and prompts the player
    to make another choice if the reponse does not match.

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function getComputerChoice(){
     return computerChoice='paper'
   }
 }
+
+let humanInput = prompt("Whats your choice? Rock, paper or scissors?");
+
+function getHumanChoice(){
+  let humanChoice = humanInput;
+  if (typeof humanChoice === 'string' || humanChoice instanceof String){
+    humanChoice=humanChoice.toLowerCase();
+    if (humanChoice.match(/^(rock|paper|scissors)$/)){
+      return humanChoice; 
+    }else {
+      return console.log("Invalid input, please try again.");
+    }
+  }else {
+    return console.log("Invalid input, please try again");
+  }
+}
