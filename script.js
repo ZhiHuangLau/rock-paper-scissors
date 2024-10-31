17e12ef38b0c59d3e673e77fb9af3d8b8ca3ee3b
ZhiHuangLau <lauzhihuang@gmail.com>
2024-10-31 16:51:33 +0800

    Adds computer response for response
    This commit adds the computer's choice for the game. Choices are randomly
    generated and has been verified on the console.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!")
+function getComputerChoice(){
+  let randNum=3*Math.random();
+  if (randNum <= 1){
+    return computerChoice='scissors';
+  } else if (randNum >2){
+    return computerChoice='rock';
+  } else{
+    return computerChoice='paper'
+  }
+}
